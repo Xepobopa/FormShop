b0e16bb7392254739c5318c064acf65e9d64fa63
daniilvinn <86977774+daniilvinn@users.noreply.github.com>
2022-09-04 14:36:52 +0300

    Added sign in form (WIP)

---
+++ Cursa4_Test/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Cursa4_Test
+{
+    partial class SignIn
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
+            this.Login_TB = new System.Windows.Forms.TextBox();
+            this.Password_TB = new System.Windows.Forms.TextBox();
+            this.SignIn_BTN = new System.Windows.Forms.Button();
+            this.Register_BTN = new System.Windows.Forms.Button();
+            this.Exit_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Login_TB
+            // 
+            resources.ApplyResources(this.Login_TB, "Login_TB");
+            this.Login_TB.Name = "Login_TB";
+            // 
+            // Password_TB
+            // 
+            resources.ApplyResources(this.Password_TB, "Password_TB");
+            this.Password_TB.Name = "Password_TB";
+            // 
+            // SignIn_BTN
+            // 
+            resources.ApplyResources(this.SignIn_BTN, "SignIn_BTN");
+            this.SignIn_BTN.Name = "SignIn_BTN";
+            this.SignIn_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Register_BTN
+            // 
+            resources.ApplyResources(this.Register_BTN, "Register_BTN");
+            this.Register_BTN.Name = "Register_BTN";
+            this.Register_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Exit_BTN
+            // 
+            resources.ApplyResources(this.Exit_BTN, "Exit_BTN");
+            this.Exit_BTN.Name = "Exit_BTN";
+            this.Exit_BTN.UseVisualStyleBackColor = true;
+            // 
+            // SignIn
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Exit_BTN);
+            this.Controls.Add(this.Register_BTN);
+            this.Controls.Add(this.SignIn_BTN);
+            this.Controls.Add(this.Password_TB);
+            this.Controls.Add(this.Login_TB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SignIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Login_TB;
+        private TextBox Password_TB;
+        private Button SignIn_BTN;
+        private Button Register_BTN;
+        private Button Exit_BTN;
+    }
+}
---
+++ Cursa4_Test/Form1.cs
@@ -0,0 +1,10 @@
+namespace Cursa4_Test
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
